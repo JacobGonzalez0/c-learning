b322136946fe7c48300bc7914a92d24b48760573
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2024-02-06 23:29:26 -0600

    Fixed parsing and main menu added

--- phoneBook/main.c
+++ phoneBook/main.c
@@ -10,7 +10,7 @@ typedef struct
 {
     char firstName[MAX_CHAR_LENGTH];
     char lastName[MAX_CHAR_LENGTH];
-    double number;
+    long int number;
     char twitter[MAX_CHAR_LENGTH];
     char discord[MAX_CHAR_LENGTH];
     char website[MAX_CHAR_LENGTH];
@@ -25,7 +25,9 @@ typedef struct
 
 typedef struct
 {
+    int totalContacts;
     Contact* contacts;
+    int totalGroups;
     Group* groups;
 } Data;
 
@@ -43,6 +45,7 @@ typedef enum
 
 
 char* readFile(int* size);
+Mode mainMenu(Data* data);
 void loadContacts(Data* data);
 void saveContacts();
 void newContact();
@@ -51,8 +54,13 @@ void cleanup();
 
 int main(void)
 {
-    Data data;
+    //init ncurses
+    initscr(); 
+    curs_set(1); 
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, FALSE); 
 
+    Data data;
     loadContacts(&data);
 
     bool running = TRUE;
@@ -62,18 +70,35 @@ int main(void)
     {
         switch(mode)
         {
-            case MAINMENU:
+            case MAINMENU: mode = mainMenu(&data);
                 break;
         }
+        running = FALSE;
     }
 
-    //cleanup
+    // cleanup
     free(data.contacts);
     free(data.groups);
+    endwin(); 
     return 0;
 }
 
+Mode mainMenu(Data* data)
+{
+    Mode mode;
+    int option;
 
+    
+
+    clear();
+    printw("Welcome to contacts!\n");
+    printw("Groups: %d \n", data->totalGroups);
+    printw("Contacts: %d \n", data->totalContacts );
+    
+    refresh();
+
+    scanw("%d", &option);
+}
 
 void loadContacts(Data* data)
 {
@@ -83,8 +108,7 @@ void loadContacts(Data* data)
 
     if(file == NULL)
     {
-        free(file);
-        return NULL;
+        return;
     }
 
     // read though array and find total contacts and groups
@@ -92,13 +116,8 @@ void loadContacts(Data* data)
     bool flag = FALSE;
     int groupSize = 1; // we assume the first line is a group 
     int contactSize = 0;
-    for(int c = 0; c > size; c++)
+    for(int c = 0; c < size; c++)
     {
-
-        if(file[c] == '\n')
-        {
-            flag = TRUE;
-        }
 
         if(flag)
         {
@@ -111,6 +130,13 @@ void loadContacts(Data* data)
             }
             flag = FALSE;
         }
+
+        if(file[c] == '\n')
+        {
+            flag = TRUE;
+        }
+
+        
         
     }
     
@@ -122,14 +148,40 @@ void loadContacts(Data* data)
     char state = 's'; // states are start = s, and then first char of linebreak
     char buffer[MAX_CHAR_LENGTH];
     // reusing variables because im insane
+    int b = 0;
     contactSize = 0;
     groupSize = 0;
     int mode = 0; // just to keep track of the order of the contacts data
-    for(int c = 0; c > size; c++)
+    for(int c = 0; c < size; c++)
     {   
 
         if(file[c] == ':')
         {
+            switch(state)
+            {
+                case 'c': 
+                    switch(mode)
+                    {
+                        case 0: strcpy(contacts[contactSize].firstName, buffer);
+                            break;
+                        case 1: strcpy(contacts[contactSize].lastName, buffer);
+                            break;
+                        case 2: contacts[contactSize].number = atoi(buffer);
+                            break;
+                        case 3: strcpy(contacts[contactSize].twitter, buffer);
+                            break;
+                        case 4: strcpy(contacts[contactSize].discord, buffer);
+                            break;
+                        case 5: strcpy(contacts[contactSize].website, buffer);
+                            break;
+                        case 6: contacts[contactSize].groupId = atoi(buffer);
+                            break;
+                    }
+                    break;
+                case 'g': strcpy(groups[groupSize].name, buffer); // only one field 
+                        groups[groupSize].id = groupSize; // we are not relying on position in array
+                    break;
+            }
             mode++;
             if(mode == CONTACT_FIELD_TOTAL)
             {
@@ -140,34 +192,9 @@ void loadContacts(Data* data)
 
         switch(state)
         {
-            case 'c': 
-                switch(mode)
-                {
-                    case 0: strcpy(contacts[contactSize].firstName, buffer);
-                        break;
-                    case 1: strcpy(contacts[contactSize].lastName, buffer);
-                        break;
-                    case 2:  char* endPtr;
-                        contacts[contactSize].number = strtod(buffer, &endPtr);
-                        if(!(endPtr != buffer)) // didnt parse number correctly
-                        {
-                            printw("Invalid number for contact: %s", contacts[contactSize].firstName);
-                            scanw("%c", NULL);
-                            refresh();
-                        }
-                        break;
-                    case 3: strcpy(contacts[contactSize].twitter, buffer);
-                        break;
-                    case 4: strcpy(contacts[contactSize].discord, buffer);
-                        break;
-                    case 5: strcpy(contacts[contactSize].website, buffer);
-                        break;
-                    case 6: contacts[contactSize].groupId = atoi(buffer);
-                        break;
-                }
+            case 'c': buffer[b] = file[c];
                 break;
-            case 'g': strcpy(groups[groupSize].name, buffer); // only one field 
-                      groups[groupSize].id = groupSize; // we are not relying on position in array
+            case 'g': buffer[b] = file[c];
                 break;
         }
 
@@ -176,7 +203,7 @@ void loadContacts(Data* data)
             state = file[c];
         }
         // End of line resets the state
-        if(c == '\n')
+        if(file[c] == '\n')
         {  
             switch(state)
             {
@@ -192,42 +219,46 @@ void loadContacts(Data* data)
 
     data->contacts = contacts;
     data->groups = groups;
-
+    data->totalContacts = contactSize;
+    data->totalGroups = groupSize;
+    
     free(file);
 
-    return contacts;
 }
 
 char* readFile(int* size)
-{   
+{
     FILE *fh;
-    fh = fopen("contact.txt", "r");
+    fh = fopen("contacts.txt", "r");
 
     if(fh == NULL)
     {
-        printw("Cannot read quiz data (quiz.txt)");
-        scanw("%c", NULL);
+        printw("Cannot read quiz data (contacts.txt)\n");
+        getch(); 
         refresh();
-
         endwin(); 
-        return;
+        return NULL; 
     }
 
     fseek(fh, 0, SEEK_END);
     *size = ftell(fh);
     rewind(fh);
-    char* buffer = (char*)malloc(size + 1);
+
+
+    char* buffer = (char*)malloc(*size + 1); 
     if(buffer == NULL)
     {
         perror("Failed to allocate memory");
+        endwin();
         return NULL;
     }
 
-    fread(buffer, size, 1, fh);
+    fread(buffer, 1, *size, fh); 
     buffer[*size] = '\0';
 
     fclose(fh);
 
     return buffer;
 }
+
 
